d4016894b644bf5cf16f1d86e9ea0f9480524132
elyasprba <elyasprba@Ts-MacBook-Pro.local>
2024-09-08 21:13:49 +0700

    fix: error redis

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -3,16 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// const client = createClient({
-//   url: process.env.REDIS_URL,
-// });
-
 const client = createClient({
-  password: process.env.REDIS_PASSWORD,
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : undefined,
-  },
+  url: process.env.REDIS_URL,
+
+  // password: process.env.REDIS_PASSWORD,
+  // socket: {
+  //   host: process.env.REDIS_HOST,
+  //   port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : undefined,
+  // },
 });
 
 const connectRedis = async () => {
