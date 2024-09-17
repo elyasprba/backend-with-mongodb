f1d0115dee2490e1a98f083dbbe133ed32b5eff0
el <95122504+elyasprba@users.noreply.github.com>
2024-09-17 15:50:32 +0700

    feat: create product (#21)
    Co-authored-by: elyasprba <elyasprba@Ts-MacBook-Pro.local>

---
+++ src/models/product.model.ts
@@ -0,0 +1,34 @@
+import mongoose, { model } from 'mongoose';
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const ProductModel = model('Product', ProductSchema);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import expesss from 'express';
 import authRouter from './auth.route';
 import userRouter from './user.route';
 import paymentRouter from './payment.route';
+import productRouter from './product.route';
 import publicRouter from './public.route';
 
 const router = expesss.Router();
@@ -12,5 +13,6 @@ router.use(publicRouter);
 router.use('/api/auth', authRouter);
 router.use('/api/user', userRouter);
 router.use('/api/payment', paymentRouter);
+router.use('/api/product', productRouter);
 
 export default router;
